e5e8e4556593df2ca53b84a763e4a393daa57f00
javser <34738549+javser@users.noreply.github.com>
2022-12-09 23:11:34 +0300

    1 file modified

--- log.js
+++ log.js
@@ -1,7 +1,8 @@
 export function log(data) {
   if (!window.logger) {
     window.logger = document.body.appendChild(document.createElement('pre'));
-    logger.style = 'outline:1px dashed #ccc';
+    logger.style =
+      'outline:1px dashed #ccc; position: fixed; bottom:0vh; width:100%; height: 20vh; overflow:scroll';
   }
   let tData = '';
   if (typeof data === 'object') {
